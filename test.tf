f016f52b845c090c0aec592379241dfe3d270fe5
Sush <sushk@gmail.com>
2023-10-29 15:29:52 +0530

    test2

--- test.tf
+++ test.tf
@@ -1,76 +1,14 @@
-# Initialize your Terraform configuration.
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">= 3.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-east-1" # Change to your desired AWS region
+  region = "us-east-1"  # Change this to your desired AWS region
 }
 
-# Create the VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-}
+# Define your EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0645cf88151eb2007"  # Replace with the AMI ID of your choice
+  instance_type = "t2.micro"  # Change to your desired instance type
+  key_name      = "Sushdemo.pem"  # Change to your SSH key pair name
 
-# Create a public subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a" # Change to your desired AZ
-  map_public_ip_on_launch = true
-}
-
-# Create a private subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b" # Change to your desired AZ
-}
-
-# Create an Internet Gateway for the VPC
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-# Attach the Internet Gateway to the VPC
-resource "aws_vpc_attachment" "my_vpc_attachment" {
-  vpc_id             = aws_vpc.my_vpc.id
-  internet_gateway_id = aws_internet_gateway.my_igw.id
-}
-
-# Create a route table for the public subnet
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-# Create a default route to the Internet Gateway in the public route table
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.my_igw.id
-}
-
-# Associate the public route table with the public subnet
-resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
+  tags = {
+    Name = "example-instance"
+  }
 }
